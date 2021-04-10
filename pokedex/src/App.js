6d142fcea6ac1fc0707b09e86a92e46f9b0ad610
Lucas Saliba <lucassaliba10@gmail.com>
2021-04-10 20:49:45 -0300

    estrutura de rotas

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,5 +1,19 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Pokedex from "./pages/Pokedex/Pokedex";
+import Pokemon from "./pages/Pokemon/Pokemon";
+
 function App() {
-  return <h1>Pokedex</h1>;
+  return (
+    <Switch>
+      <Route exact path="/" render={(props) => <Pokedex {...props} />} />
+      <Route
+        exact
+        path="/:pokemonId"
+        render={(props) => <Pokemon {...props} />}
+      />
+    </Switch>
+  );
 }
 
 export default App;
---
+++ pokedex/src/pages/Pokedex/Pokedex.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Pokedex = () => {
+  return <di>This is the Pokedex page</di>;
+};
+
+export default Pokedex;
